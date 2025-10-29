164bfee05c30c219c065518d78db67cb8eb82342
Saint. Clever <parchment@gmx.com>
2025-10-29 15:46:25 -0400

    Add Entry, Navbar components, App.jsx, data.json, index.jsx, html and css file(s)

---
+++ travel-journal/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/Navbar.jsx";
+import { Entry } from "./components/Entry.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Entry />
+    </>
+  )
+}
+
+export { App };
