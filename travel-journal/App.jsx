255a65018c13edfc12cda56e9d4b099531016f40
Saint. Clever <parchment@gmx.com>
2025-10-29 21:12:54 -0400

    Update Entry component, data, styles and App.jsx

--- travel-journal/App.jsx
+++ travel-journal/App.jsx
@@ -1,11 +1,29 @@
 import { Navbar } from "./components/Navbar.jsx";
 import { Entry } from "./components/Entry.jsx";
+import data from "./data.json";
 
 function App() {
+  let entries = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        img={entry.img.src}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+
+        // Or
+        // entry={entry}
+      />
+    )
+  })
+
   return (
     <>
       <Navbar />
-      <Entry />
+      {entries}
     </>
   )
 }
--- travel-journal/components/Entry.jsx
+++ travel-journal/components/Entry.jsx
@@ -1,45 +1,21 @@
-import data from "../data.json";
-
 let Entry = ((props) => {
-  // let entries = data.map((entry) => {
-  //   console.log(entry)
-  // })
-
   return(
     <main>
       <article className="border">
         <div className="main-image-container">
-          <img className="main-image" src="https://scrimba.com/links/travel-journal-australia-image-url"/>
+          <img className="main-image" src={props.img} alt={props.alt}/>
         </div>
         <div>
           <p>
             <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z" fill="#F55A5A"/>
-</svg> <span className="country-name">AUSTRALIA</span> <a className="google-link" href="http://">View on Google Maps</a>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z" fill="#F55A5A"/>
+            </svg> <span className="country-name">{props.country}</span>
+            <a className="google-link" href={props.googleMapsLink} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
           </p>
-          
-          <h1 className="title">Sydney Opera House</h1>
-          <p className="date">27 May, 2023 - 8 Jun, 2023</p>
-          <p className="copy">
-            The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.
-          </p>
-        </div>
-      </article>
-            <article className="border">
-        <div className="main-image-container">
-          <img className="main-image" src="https://scrimba.com/links/travel-journal-australia-image-url"/>
-        </div>
-        <div>
-          <p>
-            <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z" fill="#F55A5A"/>
-</svg> <span className="country-name">AUSTRALIA</span> <a className="google-link" href="http://">View on Google Maps</a>
-          </p>
-          
-          <h1 className="title">Sydney Opera House</h1>
-          <p className="date">27 May, 2023 - 8 Jun, 2023</p>
-          <p className="copy">
-            The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.
+          <h1 className="title">{props.title}</h1>
+          <p className="date">{props.dates}</p>
+          <p className="text">
+            {props.text}
           </p>
         </div>
       </article>
